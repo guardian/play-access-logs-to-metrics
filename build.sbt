bfc7890025448972d15eeb37aa758c13240b0d76
Alex Dufournet <alex.dufournet@guardian.co.uk>
2026-02-06 10:33:12 +0100

    Implement SQL request with Athena
    The SQL request is hardcoded for now

--- build.sbt
+++ build.sbt
@@ -8,5 +8,7 @@ lazy val root = project
 
     scalaVersion := scala3Version,
 
+    libraryDependencies += "software.amazon.awssdk" % "athena" % "2.41.22",
+    libraryDependencies += "org.slf4j" % "slf4j-simple" % "2.0.17",
     libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test
   )
--- src/main/scala/com/gu/alb/Handler.scala
+++ src/main/scala/com/gu/alb/Handler.scala
@@ -1,5 +1,21 @@
 package com.gu.alb
 
+import com.gu.alb.athena.AthenaClientImpl
+import com.gu.alb.logs.LogServiceImpl
+import com.gu.alb.models.AppIdentity
+import org.slf4j.LoggerFactory
+
+import java.time.LocalDate
+
 class Handler:
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
   def handle(): Unit =
-    println("Hello world!")
+    val athenaClient = new AthenaClientImpl("gucdk_access_logs", "s3://my-bucket/athena-output/")
+    val logService = new LogServiceImpl(athenaClient)
+
+    val appIdentity = AppIdentity(app = "facia", stack = "frontend", stage = "PROD")
+    val day = LocalDate.of(2026, 1, 29)
+    val results = logService.calculateAggregates(appIdentity, day)
+    logger.info(results.toString)
