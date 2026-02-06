49df188fb0f6fc04f67c89b2fe4c060122bcc70f
Alex Dufournet <alex.dufournet@guardian.co.uk>
2026-02-06 15:40:16 +0100

    Implement SQL generation from routes

--- src/main/scala/com/gu/alb/Handler.scala
+++ src/main/scala/com/gu/alb/Handler.scala
@@ -4,6 +4,7 @@ import com.gu.alb.athena.AthenaClientImpl
 import com.gu.alb.logs.LogServiceImpl
 import com.gu.alb.models.AppIdentity
 import org.slf4j.LoggerFactory
+import play.routes.compiler.RoutesFileParser
 
 import java.time.LocalDate
 
@@ -14,8 +15,21 @@ class Handler:
   def handle(): Unit =
     val athenaClient = new AthenaClientImpl("gucdk_access_logs", "s3://aws-frontend-logs/athena-output/")
     val logService = new LogServiceImpl(athenaClient)
+    val routesFetcher = new RoutesFetcherImpl()
+
+    val faciaRoutesFile = "https://raw.githubusercontent.com/guardian/frontend/refs/heads/main/facia/conf/routes"
+    val routesFile = routesFetcher.fetchRoutes(faciaRoutesFile)
+    logger.info(s"Fetched routes file to ${routesFile.getAbsolutePath}")
+
+    val rules = RoutesFileParser.parse(routesFile) match
+      case Left(errors) =>
+        logger.error(s"Failed to parse routes file: ${errors.map(_.message).mkString(", ")}")
+        throw new RuntimeException("Failed to parse routes file")
+      case Right(rules) =>
+        logger.info(s"Successfully parsed routes file with ${rules.size} routes")
+        rules
 
     val appIdentity = AppIdentity(app = "facia", stack = "frontend", stage = "PROD")
     val day = LocalDate.of(2026, 1, 29)
-    val results = logService.calculateAggregates(appIdentity, day)
+    val results = logService.calculateAggregates(appIdentity, day, rules)
     logger.info(results.toString)
